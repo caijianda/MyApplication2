9ff22711c6e25ee51761dbea9b0a2e0d74990598
caijianda <154435298@qq.com>
2019-10-12 10:56:08 +0800

    等会加个冲突

--- app/src/test/java/com/example/administrator/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/administrator/myapplication/ExampleUnitTest.java
@@ -12,6 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(40, 2 + 2);
+        assertEquals(40000, 2 + 2);
     }
 }
